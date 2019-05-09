068cd4a254df18bf09b99265ab582f53dc5c6e0d
quickBobbie <scherges@gmail.com>
2019-05-09 12:50:39 +0300

    End backend

---
+++ public/app/answerModal/answerModal.controller.js
@@ -0,0 +1,12 @@
+// angular.module('boris').controller('answerModalController', [
+//     '$scope',
+//     '$rootScope',
+//     '$http',
+//     function (scope, rootScope, http) {
+//
+//     }
+// ]);
+
+function answerModalController ($scope, $mdDialog) {
+
+}
---
+++ public/app/questionList/questionList.controller.js
@@ -0,0 +1,8 @@
+angular.module('boris').controller('questionListController', [
+    '$scope',
+    '$http',
+    '$rootScope',
+    function (scope, http, rootScope) {
+
+    }
+]);
